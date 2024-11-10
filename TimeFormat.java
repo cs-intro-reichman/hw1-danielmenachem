60b291706895865bc957177e32cc3207555f78fe
danielmenachem <daniellemenachem@gmail.com>
2024-11-10 14:39:45 +0200

    bug fixed

--- TimeFormat.java
+++ TimeFormat.java
@@ -6,13 +6,13 @@ public class TimeFormat {
 		// The following statement handles the minutes part of the input.
 		// it concatenates the empty string "" with the leftmost digits.
 		String minutes = "" + args[0].charAt(3) + args[0].charAt(4);
-		if (hours<12) {
+		if (hours < 12) {
 			System.out.println(hours + ":" + minutes + " AM");
 		}
 		if (hours == 12) {
 			System.out.println (hours + ":" + minutes + " PM"); 
 		}
-		else {
+		if (hours > 12) {
 			System.out.println (hours-12 + ":" + minutes + " PM");
 		}
 	}
